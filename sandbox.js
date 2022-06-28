31b05751842d131bf16757098bdbdfe35c131dec
Kenzohfs <kenzo_sato@estudante.sc.senai.br>
2022-06-27 21:39:29 -0300

    update fazendo funcionar add dados
    Retirado o nodemon no script de start

--- sandbox.js
+++ sandbox.js
@@ -26,10 +26,32 @@ const crud = require("./crud");
     No gerenciamento do firebase:
     Criação -> Firestore database -> Criar banco de dados -> Modo de produção -> us-east1 -> Continuar
 
-    Verificar se precisar botar uma configuração como true;
+    Verificar se precisar botar uma configuração como true (atualização: botei);
+    Firestore database -> Regras -> true;
+
+    Para adicionar um dado no banco de dados:
+        Pode-se usar a função save no crud já criada
+        Crie uma função aqui chamando-a (o campo id de parâmetro serve para definir a chave primária,
+            caso seja adicionado duas pessoas com o mesmo id ele vai só substituir)
+        Chame essa função
 */
 
 // async function buscarDadoId() {
 //     const dados = await crud.getById("Pessoas", "3jJdriJs1449GtfWbcEY")
 //     console.log(dados);
 // }
+
+// async function salvarDado() {
+//     const savedData = await crud.save("Pessoas", "7U36ftZW8febuTVhP2Fi",
+//         { nome: "Kenzo", sobrenome: "Sato", idade: 17 });
+//     console.log(savedData);
+// }
+
+async function salvarDado() {
+    console.log("ta passando aq");
+    const savedData = await crud.save("Pessoas", '12345678901',
+        { nome: "teste2", idade: 23 });
+    console.log(savedData);
+}
+
+salvarDado();
